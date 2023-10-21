db33cc2c83c35cc8f2664b42bb769047179e8cb6
nataliapineda2024 <natalia.pineda2024@hilasal.superate.org.sv>
2023-10-20 21:29:50 -0600

    Nuevos cambios

--- src/history.js
+++ src/history.js
@@ -1,13 +1,37 @@
 function showHistory(){
+    window.location = "showHistory.html";
+    userAccount.history = JSON.parse(localStorage.getItem('historial'));
+  
     console.log(userAccount.history);
 
     userAccount.history.map(history => {
-        document.write("+ " , "$ " , history.Amount, "<br>");
-        document.write(history.payer, "<br>");
-        document.write(history.NumberAccount, "<br>");
-        document.write("$ " , history.balance, "<br>");
-        document.write(history.Date, "<br>");
-        document.write("<br>" , "--------------------------------------------", "<br>");    
+        const showbonus = document.createElement("p");
+        showbonus.innerText = "+ " , "$ " , history.Amount, "<br>";
+        document.getElementById("SHOWhistory").appendChild(showbonus);
+
+        const showpayer = document.createElement("p");
+        showpayer.innerText = history.payer, "<br>";
+        document.getElementById("SHOWhistory").appendChild(showpayer);
+
+        const showNumAccount = document.createElement("p");
+        showNumAccount.innerText = history.NumberAccount, "<br>";
+        document.getElementById("SHOWhistory").appendChild(showNumAccount);
+
+        const showbalance = document.createElement("p"); 
+        showbalance.innerText = "$ " , history.balance, "<br>";
+        document.getElementById("SHOWhistory").appendChild(showbalance);
+
+        const showdate = document.createElement("p");
+        showdate.innerText = history.Date, "<br>";
+        document.getElementById("SHOWhistory").appendChild(showdate);
+        
+        const showline = document.createElement("p");
+        showline.innerText = "<br>" , "--------------------------------------------", "<br>";
+        document.getElementById("SHOWhistory").appendChild(showline);
     })
+
 }
 
+function back(){
+    window.location = "index.html";
+}
